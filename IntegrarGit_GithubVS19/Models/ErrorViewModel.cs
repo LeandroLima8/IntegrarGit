14da607b172ba55f19aa1f3d8d8e80e35b2899e2
Leandro Lima <leandrolima-8@live.com.pt>
2021-07-07 14:46:29 +0100

    Add project files.

---
+++ IntegrarGit_GithubVS19/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IntegrarGit_GithubVS19.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
